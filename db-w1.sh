7dae1e1d7d3f77ead54bec97d24e503eddcb741f
Archie Brentano <isntall.us@gmail.com>
2016-09-05 20:07:05 -0700

    removing math and run scripts thru bash

--- db-w1.sh
+++ db-w1.sh
@@ -12,21 +12,11 @@ if [ "logro" = "$1" ]; then
   done
   for i in $DEVICEDIR; do
     j=${i/-/_}
-    mv ${j}.txt /tmp/${j}.txt.$(date +%s)
+    mv ./${i}.txt /tmp/${j}.txt.$(date +%s)
   done
 elif [ "dump" = "$1" ]; then
   rsync -A /tmp/*.txt.* ser@nuc5:/tmp/
-  ssh ser@nuc5 "mysqlimport ${dbCreds}  --use-threads=3 -h 127.0.0.1 w1 --local /tmp/*.txt.*"
-  ssh ser@nuc5 "rm /tmp/*.txt.*"
+  ssh ser@nuc5 "bash -c 'mysqlimport ${dbCreds}  --use-threads=3 -h 127.0.0.1 w1 --local /tmp/*.txt.*'"
+  ssh ser@nuc5 "bash -c 'rm /tmp/*.txt.*'"
   rm /tmp/*.txt.*
-else
-  touch ./active
-  for i in $DEVICEDIR; do
-    j=${i/-/_}
-    TEMPC_RAW=$(cat $W1DIR/${i}/w1_slave | grep 't=' | awk -F= '{print $2}')
-    TEMPC=$(echo "scale=4; $TEMPC_RAW/1000" | bc)
-    TEMPF=$(echo "scale=4; ($TEMPC*(9.0/5.0))+32.0" | bc)
-    echo -e "$(date +%s)\t${TEMPC}\t${TEMPF}" >> ${j}.txt
-  done
-  rm ./active
 fi
